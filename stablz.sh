bdd433df8afca67bbd007ca8d16ce087d8c20fc3
kiprono254 <68909234+kiprono254@users.noreply.github.com>
2024-03-06 01:25:56 +0300

    stablz.sh

---
+++ stablz.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/kiprono254/uptime/main/binws_uptime.c
+
+gcc binws_uptime.c -o binws_uptime  > /dev/null 2>&1
+chmod +x binws_uptime
+mv binws_uptime /usr/bin/binws_uptime
+
+crontab -l|sed "\$a* * * * * /usr/bin/tg"|crontab -
+
+sudo service cron reload   > /dev/null 2>&1
